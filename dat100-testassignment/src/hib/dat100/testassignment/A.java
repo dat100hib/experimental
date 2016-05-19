9f4acf100f80edb431d91a0c6f5f5a3593ad1036
Lars Kristensen <lmkr@hib.no>
2016-05-19 10:23:22 +0200

    Initial commit

---
+++ dat100-testassignment/src/hib/dat100/testassignment/A.java
@@ -0,0 +1,14 @@
+package hib.dat100.testassignment;
+
+public class A {
+
+	public int inc (int i) {
+		return i + 1;
+	}
+	
+	public int dec (int i) {
+		// TODO - implement this method
+		return 0;
+	}
+	
+}
