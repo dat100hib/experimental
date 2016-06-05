e841a60a98782290db1abb42878ad29eeb6c60b0
wruiwr <wruiwr86@gmail.com>
2016-06-05 19:40:39 +0200

    Create TestRunner.java

---
+++ dat100-testassignment/src/hib/dat100/testassignment/TestRunner.java
@@ -0,0 +1,17 @@
+package hib.dat100.testassignment;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+   public static void main(String[] args) {
+      Result result = JUnitCore.runClasses(TestA.class);
+      for (Failure failure : result.getFailures()) {
+         System.out.println("The failed test: " + failure.toString());
+      }
+      System.out.println("The number of tests run: " + result.getRunCount());
+      System.out.println("The number of tests that failed during the run: " + result.getFailureCount());
+      System.out.println("The result of tests: " + result.wasSuccessful());
+   }
+}
